6ba4500e1b07d12cea9357d7696774cd9de35674
Bespi123 <b_antonioesp@hotmail.com>
2025-02-18 07:53:11 -0500

    Add helmholtz coils optimizer

--- src/helmCoils_optmizer.py
+++ src/helmCoils_optmizer.py
@@ -1,7 +1,7 @@
 import numpy as np
 import random
 from deap import base, creator, tools, algorithms
-import helmCoils_simulator as test_3
+import src.helmCoils_simulator as test_3
 
 # AWG data (remains global if it is constant)
 # Constants
